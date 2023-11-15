5bb88535730e2e2dba3e739e3f9e6796f09aac9b
hm10000 <hmlim@ucsd.edu>
2023-11-14 22:13:48 -0800

    Add files

---
+++ 03-task-manager/starter/app.js
@@ -0,0 +1,36 @@
+//Hyunmin Lim
+const express = require('express')
+const app = express();
+const tasks = require('./routes/tasks.js')
+const connectDB = require('./db/connect.js')
+
+require('dotenv').config()
+const notFound = require('./middleware/not-found.js')
+const errorHandlerMiddleware = require('./middleware/errorHandler.js')
+
+//middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+
+// routes
+
+app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
