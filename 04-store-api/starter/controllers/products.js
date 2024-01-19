2248d84a3fe98a78a43b738826169e76f769bba9
Hyunmin Lim <hmlim@ucsd.edu>
2024-01-19 06:12:19 -0800

    20240119

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,30 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({name: 'vase table',})
+    const products = await Product.find({
+    }).sort('-name price')
     res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req,res) => {
-    const products = await Product.find(req.query);
+    const {featured, company, name, sort} = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === 'true' ? 'true' : 'false'
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name =  { $regex: name, $options: 'i'}
+    }
+    console.log(queryObject)
+    let result = await Product.find(queryObject)
+    if(sort) {
+        products = products.sort()
+    }
+    const products = await result
     res.status(200).json({products, nbHits:products.length})
 }
 
